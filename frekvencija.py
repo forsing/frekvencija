93a6d6eb43c5c53c30ad8513c3803034ac0fc79d
ForsIng <forsing@gmail.com>
2025-11-01 07:46:35 +0100

    Update frekvencija.py

--- frekvencija.py
+++ frekvencija.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 """
 svih 4502 izvlacenja
-30.07.1985.- 28.10.2025.
+30.07.1985.- 31.10.2025.
 """
 
 df = pd.read_csv("/data/loto7_4502_k85.csv", header=None)
@@ -32,7 +32,12 @@ print()
 print(df.tail())
 print()
 """
-
+      0   1   2   3   4   5   6
+4499  1   5   6   7  11  24  37
+4500  2   4   6  11  21  33  35
+4501  1   3  11  12  19  35  38
+4502  1   2   6  10  18  24  34
+4503  6  20  21  22  27  29  36
 """
 
 
@@ -47,14 +52,17 @@ for k, v in counter.items():
     print(f'{k} : {v}')
 print()
 """
-1 : 762
-2 : 800
-3 : 805
+1 : 764
+2 : 802
+3 : 806
+4 : 792
+5 : 801
 
+...
 
-
+36 : 767
 37 : 837
-38 : 816
+38 : 817
 39 : 825
 """
 
@@ -76,12 +84,14 @@ print()
 8 : 886
 23 : 884
 26 : 855
+34 : 843
 
+...
 
-
+1 : 764
 30 : 760
 17 : 749
-20 : 746
+20 : 747
 """
 
 
@@ -101,22 +111,24 @@ counter_h = Counter(all_numbers_h)
 
 top_39_h = counter_h.most_common(39)
 print()
-print("svih 4502 izvlacenja")
-print("30.07.1985.- 28.10.2025.")
+print("svih 4504 izvlacenja")
+print("30.07.1985.- 31.10.2025.")
 print()
 for num, cnt in top_39_h:
     print(f"{num}: {cnt}")
 print()
 """
-svih 4502 izvlacenja
-30.07.1985.- 28.10.2025.
+svih 4504 izvlacenja
+30.07.1985.- 31.10.2025.
 
 8: 886
 23: 884
 26: 855
+34: 842
 
+...
 
-
+1: 763
 30: 760
 17: 749
 20: 746
